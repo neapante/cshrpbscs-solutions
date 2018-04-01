bb7fa88be0609b8cf5f07a92a471e5387e3182e6
Noe Emmanuel Pante <neapante@gmail.com>
2018-04-01 22:54:08 +0800

    Added arrayandarraylist and constructorsanddestructors solutions

---
+++ ArrayAndArrayList/ArrayAndArrayList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAndArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array
+            //Has a fixed length and only int data type can be added 
+            int[] array1 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine(array1[0]);
+            Console.WriteLine(array1[1]);
+            Console.WriteLine(array1[2]);
+
+            //ArrayList
+            //Does not need the size to be indicated and it grows dynamically as a value added in the array list
+            //Any datatype can be added
+            ArrayList arrayList1 = new ArrayList();
+            arrayList1.Add(1);
+            arrayList1.Add(1.23);
+            arrayList1.Add("String");
+
+            Console.WriteLine(arrayList1[0]);
+            Console.WriteLine(arrayList1[1]);
+            Console.WriteLine(arrayList1[2]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstructorsAndDestructors.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndDestructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cow c = new Cow("Barney");
+            Console.ReadLine();
+        }
+    }
+
+    class Cow
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //Constructor
+        //Shoul be the same name as the Class
+        //A constructor that writes the name of the cow
+        public Cow (string cowName)
+        {
+            Name = cowName;
+            Console.WriteLine(Name);
+        }
+
+        //Destructor
+        //Same as the Class but should have the tilde sign before it
+        //Runs when the class is not reachable - https://www.dotnetperls.com/destructor
+        ~Cow()
+        {
+            Console.WriteLine("Running Destructor");
+        }
+    }
+}
